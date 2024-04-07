00c6b34c8c973fecfbb9a8ffbac141c2f09635cc
umita <umita@UMITAYKUT>
2024-04-08 00:07:33 +0300

    Add project files.

---
+++ Business/Abstracts/IBrandService.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstracts
+{
+    public interface IBrandService
+    {
+       CreateBrandResponse Add(CreateBrandResquest brand);
+        List<Brand> GetAll();
+    }
+}
+
+//responses and requests
